9128addb7a6352280eb9b89c828b189d56912ba4
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-22 18:13:17 +0800

    Instance, Membuat atribut dan memanggil method

--- src/main/java/praktek03/SepedaAksi.java
+++ src/main/java/praktek03/SepedaAksi.java
@@ -1,5 +1,12 @@
 package praktek03;
 
 public class SepedaAksi {
-    
+    public static void main(String[] args) {
+        Sepeda s = new Sepeda();
+        s.merk = "SiLaju";
+        s.warna = "Hitam";
+        s.harga = 400000;
+        
+        s.cetakInfo();
+    }
 }
