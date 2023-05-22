f6869a2b5725ca34eaee42924c888dfc17cd17f3
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-22 18:02:18 +0800

    Membuat atribut

--- src/main/java/praktek03/Sepeda.java
+++ src/main/java/praktek03/Sepeda.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package praktek03;
 
-/**
- *
- * @author Alzahsi12
- */
 public class Sepeda {
-    
+    String merk;
+    String warna;
+    double harga;
+             
 }
