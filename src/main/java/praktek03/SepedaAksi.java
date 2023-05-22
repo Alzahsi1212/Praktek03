559dfc4418e74423e79e4422d613b0e11c8cba60
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-22 18:20:37 +0800

    Menggunakan method set

--- src/main/java/praktek03/SepedaAksi.java
+++ src/main/java/praktek03/SepedaAksi.java
@@ -3,9 +3,10 @@ package praktek03;
 public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
-        s.merk = "SiLaju";
-        s.warna = "Hitam";
-        s.harga = 400000;
+        
+        s.setMerk("SiLaju");
+        s.setWarna("Hitam");
+        s.setHarga(400000);
         
         s.cetakInfo();
     }
