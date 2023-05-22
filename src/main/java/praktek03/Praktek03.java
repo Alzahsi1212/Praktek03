688975ad6ce2d31f919bdde9ca9849c7c498d5a3
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-22 18:00:15 +0800

    Initial commit

---
+++ src/main/java/praktek03/Praktek03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package praktek03;
+
+/**
+ *
+ * @author Alzahsi12
+ */
+public class Praktek03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
